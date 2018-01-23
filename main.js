5dd27c9f6d6c978f54b5925676d12432369518d0
Luke <luke@luke.co.pl>
2018-01-23 01:27:35 +0100

    Replace hardcoded element with target card, function to create a card

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
-let card = document.getElementById('card');
-
 document.addEventListener('click', e => {
     if (e.target.classList.contains('card')) {
-        card.classList.add('card-half-rotate');
+        e.target.classList.add('card-half-rotate');
     }
 }, false);
 
 document.addEventListener('transitionend', e => {
     if (e.target.classList.contains('card-half-rotate')) {
-        card.classList.remove('card-half-rotate');
-        card.classList.toggle('card-visible');
+        e.target.classList.remove('card-half-rotate');
+        e.target.classList.toggle('card-visible');
     }
 }, false);
+
+function createCard(color, value) {
+    let card = document.createElement('div');
+    card.className = `card card-${color}-${value}`;
+    document.body.appendChild(card);
+}
+
+createCard('clubs', 'A');
