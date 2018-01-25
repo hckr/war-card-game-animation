28182f9b2575c0b08d5787a008169ed3451d31bd
Luke <luke@luke.co.pl>
2018-01-25 23:29:17 +0100

    Players playing first cards; layout technical changes, e.g. flexbox to percents

--- main.js
+++ main.js
@@ -68,21 +68,68 @@ function dealCard(deck, hands, handEls, playerInd) {
 }
 
 function dealCards(deck, hands, handEls) {
-    (function f(deck, hands, handEls, player) {
-        if (deck.length == 0) {
-            return;
+    return new Promise((resolve, reject) => {
+        (function f(deck, hands, handEls, player) {
+            if (deck.length == 0) {
+                resolve();
+                return;
+            }
+            dealCard(deck, hands, handEls, player).then(
+                _ => f(deck, hands, handEls, (player + 1) % 2));
+        })(deck, hands, handEls, 0);
+    });
+}
+
+function playCard(hands, handEls, tables, tableEls, playerInd) {
+    let card = hands[playerInd].pop();
+    tables[playerInd].push(card);
+    let rect = card.elem.getBoundingClientRect();
+    document.body.appendChild(card.elem);
+    let [currTop, currLeft] = rectToAbsPercentPos(rect);
+    card.elem.style.top = currTop + '%';
+    card.elem.style.left = currLeft + '%';
+    card.elem.classList.add('card-detached');
+    return new Promise((resolve, reject) => {
+        let rect = tableEls[playerInd].getBoundingClientRect();
+        let [newTop, newLeft] = rectToAbsPercentPos(rect);
+        function onTransitionEnd() {
+            tableEls[playerInd].appendChild(card.elem);
+            card.elem.removeEventListener('transitionend', onTransitionEnd, false);
+            card.elem.classList.remove('card-detached');
+            card.elem.style.top = null;
+            card.elem.style.left = null;
+            card.elem.style.transform = null;
+            resolve(card);
         }
-        dealCard(deck, hands, handEls, player).then(
-            _ => f(deck, hands, handEls, (player + 1) % 2));
-    })(deck, hands, handEls, 0);
+        card.elem.addEventListener('transitionend', onTransitionEnd, false);
+        card.elem.style.top = newTop + '%';
+        card.elem.style.left = newLeft + '%';
+        card.elem.style.transform = 'rotate(0)';
+    });
 }
 
 const suits = ['hearts', 'diamonds', 'clubs', 'spades'];
 const ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 
 function gameAnimation(parent) {
-    let animRootEl = document.createElement('div');
+    let animRootEl = document.createElement('div'),
+        leftHandWrapperEl = document.createElement('div'),
+        tableWrapperEl = document.createElement('div'),
+        leftTableWrapperEl = document.createElement('div'),
+        rightTableWrapperEl = document.createElement('div'),
+        rightHandWrapperEl = document.createElement('div');
+
     animRootEl.className = 'card-anim-root';
+    leftHandWrapperEl.className = 'hand-wrapper';
+    tableWrapperEl.className = 'table-wrapper';
+    leftTableWrapperEl.className = 'player-table-wrapper';
+    rightTableWrapperEl.className = 'player-table-wrapper';
+    rightHandWrapperEl.className = 'hand-wrapper';
+
+    animRootEl.appendChild(leftHandWrapperEl);
+    animRootEl.appendChild(tableWrapperEl);
+    animRootEl.appendChild(rightHandWrapperEl);
+    parent.appendChild(animRootEl);
 
     let deck = [],
         deckEl = document.createElement('div');
@@ -103,19 +150,37 @@ function gameAnimation(parent) {
 
     let numPlayers = 2,
         hands = [[], []],
-        handEls = [];
+        handEls = [],
+        tables = [[], []],
+        tableEls = [];
 
     for (let i = 0; i < numPlayers; ++i) {
-        let el = document.createElement('div');
-        el.className = 'hand';
-        handEls.push(el);
+        let handEl = document.createElement('div');
+        handEl.className = 'hand';
+        handEls.push(handEl);
+
+        let tableEl = document.createElement('div');
+        tableEl.className = 'player-table';
+        tableEls.push(tableEl);
     }
 
-    animRootEl.appendChild(handEls[0]);
-    animRootEl.appendChild(deckEl);
-    animRootEl.appendChild(handEls[1]);
+    leftHandWrapperEl.appendChild(handEls[0]);
+    tableWrapperEl.appendChild(deckEl);
+    leftTableWrapperEl.appendChild(tableEls[0]);
+    rightTableWrapperEl.appendChild(tableEls[1]);
+    rightHandWrapperEl.appendChild(handEls[1]);
 
-    parent.appendChild(animRootEl);
+    dealCards(deck, hands, handEls).then(
+        _ => {
+            tableWrapperEl.innerHTML = '';
+            tableWrapperEl.appendChild(leftTableWrapperEl);
+            tableWrapperEl.appendChild(rightTableWrapperEl);
 
-    dealCards(deck, hands, handEls);
+            setTimeout(_ => {
+                playCard(hands, handEls, tables, tableEls, 0).then(
+                    card => card.turnOver());
+                playCard(hands, handEls, tables, tableEls, 1).then(
+                    card => card.turnOver());
+            }, 500);
+        });
 }
